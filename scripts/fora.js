9b8d643c6394ea3be5a6f104380b03cbf13edec5
Jay <Acorder43812@gmail.com>
2022-07-20 01:57:19 -0700

    Update

--- scripts/fora.js
+++ scripts/fora.js
@@ -15,8 +15,8 @@ const highlightMenu = () => {
   const elem = document.querySelector('.highlight');
   const homeMenu = document.querySelector('#home-page');
   const aboutMenu = document.querySelector('#about-page');
-  const servicesMenu = document.querySelector('#services-page'); 
-  const signupMenu = document.querySelector('#sign-up'); 
+  const featuresMenu = document.querySelector('#features-page'); 
+  const supportMenu = document.querySelector('#support-page'); 
   let scrollPos = window.scrollY;
   // console.log(scrollPos);
 
@@ -24,14 +24,25 @@ const highlightMenu = () => {
   if (window.innerWidth > 960 && scrollPos < 600) {
     homeMenu.classList.add('highlight');
     aboutMenu.classList.remove('highlight');
+    featuresMenu.classList.remove('highlight');
+    supportMenu.classList.remove('highlight');
     return;
   } else if (window.innerWidth > 960 && scrollPos < 1400) {
     aboutMenu.classList.add('highlight');
     homeMenu.classList.remove('highlight');
-    servicesMenu.classList.remove('highlight');
+    featuresMenu.classList.remove('highlight');
+    supportMenu.classList.remove('highlight');
     return;
   } else if (window.innerWidth > 960 && scrollPos < 2345) {
-    servicesMenu.classList.add('highlight');
+    featuresMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    supportMenu.classList.remove('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2900) {
+    featuresMenu.classList.remove('highlight');
+    homeMenu.classList.remove('highlight');
+    supportMenu.classList.add('highlight');
     aboutMenu.classList.remove('highlight');
     return;
   }
